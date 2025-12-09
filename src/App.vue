a2863c01227e13288cdc07ff433e5eb3ca5b391f
HumaneLogic <forgodforlogic@gmail.com>
2025-12-09 19:40:31 +0330

    add supabase

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { supabase } from '@/lib/supabaseClient'
+console.log(supabase)
 </script>
 
 <template>
@@ -7,3 +8,9 @@ import { RouterView } from 'vue-router'
     <RouterView />
   </main>
 </template>
+
+<!--
+ 1.SPA: RouterView from vue-router - vue-router maps components to routs - uses the browser history api to handle navigating between pages without reloading the entire page
+ 2.lazy loading  - dynamic imports - load components only when needed
+ 3.file-based routing
+-->
