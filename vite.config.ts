99a299d6bb91470f5b201786319a5f91374c3936
HumaneLogic <forgodforlogic@gmail.com>
2025-12-12 13:09:00 +0330

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     VueRouter(),
     vue(),
+    tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
