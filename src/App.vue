85c5f1b29771c70dc7164f121d8f696aa173214b
HumaneLogic <forgodforlogic@gmail.com>
2025-12-14 22:43:03 +0330

    fix search icon

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import { Input } from '@/components/ui/input'
     <form class="relative h-fit w-full max-w-96">
       <iconify-icon
         class="absolute top-[50%] translate-y-[-50%] left-2.5"
-        icon="lucide:sea"
+        icon="lucide:search"
       ></iconify-icon>
 
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
