f850652e043d186806ff44aec34c0be47b284945
HumaneLogic <forgodforlogic@gmail.com>
2025-12-15 20:59:59 +0330

    extract home page into AuthLayout and improve it

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-// import { supabase } from '@/lib/supabaseClient'
-// console.log(supabase)
-import Sidebar from '@/components/Layout/Sidebar.vue'
-import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import AuthLayout from '@/components/Layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
-
-<!--
- 1.SPA: RouterView from vue-router - vue-router maps components to routs - it uses the browser history api to handle navigating between pages without reloading the entire page
- 2.lazy loading  - dynamic imports - load components only when needed
- 3.file-based routing
--->
