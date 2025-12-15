e99ea06194817ed424f63415552056b02c4c2a0d
HumaneLogic <forgodforlogic@gmail.com>
2025-12-15 22:07:05 +0330

    add tasks table

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,6 @@
     <h1>Home Page</h1>
     <RouterLink :to="{ name: '/projects/[id]', params: { id: 'default' } }">Projects</RouterLink>
     <RouterLink :to="'/tasks'">Tasks</RouterLink>
-    <!-- <RouterLink to="/projects">Go to Projects</RouterLink> -->
     <RouterLink to="/about">About</RouterLink>
   </div>
 </template>
