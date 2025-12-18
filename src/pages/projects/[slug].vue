4c5dd9921537f2e297b399434f5262a9c3958f2b
HumaneLogic <forgodforlogic@gmail.com>
2025-12-19 01:55:38 +0330

    fetch data for individual project page

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,10 +1,116 @@
 <script setup lang="ts">
-const route = useRoute()
-</script>
+import { projectQuery, type Project } from '@/utils/supaQueries'
 
+const route = useRoute('/projects/[slug]')
+const project = ref<Project | null>(null)
+
+const getProjects = async () => {
+  const { data, error } = await projectQuery(route.params.slug)
+  if (error) console.log(error)
+  project.value = data
+}
+await getProjects()
+</script>
+<!--
 <template>
   <div>
     <h1>Project {{ route.params?.slug }}</h1>
     <RouterLink to="/">Home</RouterLink>
   </div>
+</template> -->
+
+<template>
+  <div>
+    <Table>
+      <TableRow>
+        <TableHead> Name </TableHead>
+        <TableCell>
+          <AppInPlaceEditText />
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Description </TableHead>
+        <TableCell>
+          <AppInPlaceEditTextarea />
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Status </TableHead>
+        <TableCell>
+          <AppInPlaceEditStatus />
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Collaborators </TableHead>
+        <TableCell>
+          <div class="flex">
+            <Avatar class="-mr-4 border border-primary hover:scale-110 transition-transform">
+            </Avatar>
+          </div>
+        </TableCell>
+      </TableRow>
+    </Table>
+
+    <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+      <div class="flex-1">
+        <h2>Tasks</h2>
+        <div class="table-container">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead> Name </TableHead>
+                <TableHead> Status </TableHead>
+                <TableHead> Due Date </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell class="p-0"> </TableCell>
+                <TableCell>
+                  <AppInPlaceEditStatus />
+                </TableCell>
+                <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <div class="flex-1">
+        <h2>Documents</h2>
+        <div class="table-container">
+          <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
+            This project doesn't have documents yet...
+          </p>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead> Name </TableHead>
+                <TableHead> Visibility </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+                <TableCell> Private </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<style scoped>
+th {
+  @apply w-25;
+}
+
+h2 {
+  @apply mb-4 text-lg font-semibold w-fit;
+}
+
+.table-container {
+  @apply overflow-hidden overflow-y-auto rounded-md bg-slate-900 h-80;
+}
+</style>
--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -13,3 +13,26 @@ export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 export const projectsQuery = supabase.from('projects').select()
 export type Projects = QueryData<typeof projectsQuery>
+
+// query the 'projects' table, use select to select properties
+// we need to pass the slug dynamically whenever we trigger the query by making the constant a function
+export const projectQuery = (slug: string) =>  supabase.from('projects').select(`
+  *,
+  tasks (
+    id,
+    name,
+    status,
+    due_date
+  )
+`)
+.eq('slug', slug)
+.single()
+// give data for only the row that has 'slug' value equals the second argument(some-slug)
+
+//  get only the project that corresponds with page that the user is visiting
+//  eq: get a specific row from a table
+
+//  we get only the function type with this
+// export type Project = QueryData<typeof projectQuery>
+// we want whatever this function returns which is the query statement
+export type Project = QueryData<ReturnType<typeof projectQuery>>
