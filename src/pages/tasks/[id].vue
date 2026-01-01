9fde5bb9139e14c125b5b491c9c6b0bfde2f70b6
HumaneLogic <forgodforlogic@gmail.com>
2026-01-02 03:08:15 +0330

    add dynamic individual task page

--- src/pages/tasks/[id].vue
+++ src/pages/tasks/[id].vue
@@ -1,4 +1,137 @@
 <script setup lang="ts">
+import { taskQuery, type Task } from '@/utils/supaQueries'
+
+// define a ref to store the task data
+// write a function to fetch the task
+// set up supabase query and define the query type
+// use watch to update the page title
+// update the template with the fetched data
+
+const route = useRoute('/tasks/[id]')
+const task = ref<Task | null>(null)
+
+watch(
+  () => task.value?.name,
+  () => {
+    usePageStore().pageData.title = `Task: ${task.value?.name || ''}`
+  },
+)
+
+const getTask = async () => {
+  const { data, error } = await taskQuery(Number(route.params.id))
+  if (error) console.log(error)
+  task.value = data
+}
+await getTask()
+</script>
+
+<template>
+  <div>
+    <Table v-if="task">
+      <TableRow>
+        <TableHead> Name </TableHead>
+        <TableCell>
+          {{ task.name }}
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Description </TableHead>
+        <TableCell>
+          {{ task.description }}
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Assignee </TableHead>
+        <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Project </TableHead>
+        <TableCell> {{ task.projects?.name }} </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Status </TableHead>
+        <TableCell> {{ task.status }} </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableHead> Collaborators </TableHead>
+        <TableCell>
+          <div class="flex">
+            <Avatar
+              class="-mr-4 border border-primary hover:scale-110 transition-transform"
+              v-for="collab in task.collaborators"
+              :key="collab"
+            >
+              <RouterLink class="w-full h-full flex items-center justify-center" to="">
+                <AvatarImage src="" alt="" />
+                <AvatarFallback> </AvatarFallback>
+              </RouterLink>
+            </Avatar>
+          </div>
+        </TableCell>
+      </TableRow>
+      <TableRow class="hover:bg-transparent">
+        <TableHead class=""> Comments </TableHead>
+        <TableCell>
+          Comments goes in here ...
+          <div class="flex flex-col justify-between p-2 bg-muted my-3 rounded-md">
+            <textarea
+              placeholder="Add your comment..."
+              class="w-full max-w-full overflow-y-auto prose-sm border rounded dark:prose-invert hover:border-muted bg-background border-muted"
+            ></textarea>
+            <div class="flex justify-between mt-3">
+              <Button> Comment </Button>
+              <div class="flex gap-4">
+                <button variant="ghost" @click.prevent>
+                  <iconify-icon icon="wpf:attach"></iconify-icon>
+                  <span class="sr-only">Attach file</span>
+                </button>
+                <button variant="ghost" @click.prevent>
+                  <iconify-icon icon="uil:image-upload"></iconify-icon>
+                  <span class="sr-only">Upload image</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </TableCell>
+      </TableRow>
+    </Table>
+
+    <!-- <Button
+      @click="triggerDelete"
+      class="self-end mt-3 w-full max-w-40"
+      variant="destructive"
+    >
+      <Transition name="scale" mode="out-in">
+        <iconify-icon
+          v-if="deleteLoading"
+          icon="lucide:loader-circle"
+          class="mr-1 animate-spin"
+        ></iconify-icon>
+        <iconify-icon v-else icon="lucide:trash-2" class="mr-1"></iconify-icon>
+      </Transition>
+      Delete Task
+    </Button> -->
+  </div>
+</template>
+
+<style scoped>
+@reference "tailwindcss";
+@reference "../../assets/style.css";
+
+th {
+  @apply w-25;
+}
+
+h2 {
+  @apply mb-4 text-lg font-semibold w-fit;
+}
+
+.table-container {
+  @apply overflow-hidden overflow-y-auto rounded-md bg-slate-900 h-80;
+}
+</style>
+
+<!-- <script setup lang="ts">
 const route = useRoute()
 </script>
 
@@ -7,4 +140,4 @@ const route = useRoute()
     <h1>Task {{ route.params?.id }}</h1>
     <RouterLink to="/">Home</RouterLink>
   </div>
-</template>
+</template> -->
--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -27,12 +27,30 @@ export const projectQuery = (slug: string) =>  supabase.from('projects').select(
 `)
 .eq('slug', slug)
 .single()
-// give data for only the row that has 'slug' value equals the second argument(some-slug)
+// give data for only the row that has 'slug' value equals the second argument(slug)
 
-//  get only the project that corresponds with page that the user is visiting
+//  get only the project that corresponds with the page that the user is visiting
 //  eq: get a specific row from a table
 
 //  we get only the function type with this
 // export type Project = QueryData<typeof projectQuery>
 // ReturnType tells typescript that we don't want to get the function type but we want whatever this function returns, which is the query statement
 export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: number) => {
+  return supabase
+    .from('tasks')
+    .select(
+      `
+      *,
+      projects (
+        id,
+        name,
+        slug
+      )
+    `
+    )
+    .eq('id', id)
+    .single()
+}
+export type Task = QueryData<ReturnType<typeof taskQuery>>
