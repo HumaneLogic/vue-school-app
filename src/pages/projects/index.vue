ef6c7e65e85e48e95ff866415d31be7c088a4999
HumaneLogic <forgodforlogic@gmail.com>
2026-01-12 08:54:20 +0330

    use pinia for efficient data loading and caching

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects | null>(null)
+const projectsLoader = useProjectsStore()
 
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery
-  if (error) useErrorStore().setError({ error, customCode: status })
-  projects.value = data
-}
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
+
 await getProjects()
 </script>
 
---
+++ src/stores/loaders/projects.ts
@@ -0,0 +1,20 @@
+import { type Projects, projectsQuery } from '@/utils/supaQueries'
+
+export const useProjectsStore = defineStore('projects-store', () => {
+  const projects = ref<Projects | null>(null)
+
+  const getProjects = async () => {
+    if (projects.value?.length) return
+
+    const { data, error, status } = await projectsQuery
+
+    if (error) useErrorStore().setError({ error, customCode: status })
+
+    projects.value = data
+  }
+
+  return {
+    projects,
+    getProjects,
+  }
+})
