cc2c2936a99cdbc2924c071d4d1e09cf87a90c0f
HumaneLogic <forgodforlogic@gmail.com>
2026-01-12 09:41:14 +0330

    add caching technic: stale while revalidate

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -3,7 +3,25 @@ import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
+
+  // query database and cache it reactively
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
+
+  // query database and compare with cache: if cache is old, show it but use new data on the next navigation to the projects page
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        // projects.value is the cached data
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('The cached data and the data from database query, match completely')
+          return
+        } else {
+          console.log('There is new data from database')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
 
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects')
@@ -11,6 +29,8 @@ export const useProjectsStore = defineStore('projects-store', () => {
     if (error) useErrorStore().setError({ error, customCode: status })
 
     projects.value = data
+
+    validateCache()
   }
 
   return {
